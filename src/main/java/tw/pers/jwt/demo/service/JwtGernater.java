fed2d1d5b51fc362b5c766c756f39584e1cfb06b
big67666 <qd3111555@gmail.com>
2024-02-23 23:46:54 +0800

    更新:用以前得到的亂碼測試

--- src/main/java/tw/pers/jwt/demo/service/JwtGernater.java
+++ src/main/java/tw/pers/jwt/demo/service/JwtGernater.java
@@ -1,10 +1,13 @@
 package tw.pers.jwt.demo.service;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtGernater {
-
+    private final static String SECRET_KEY="443185454aa1d224ae5fa6a04847d7c690abe7eac0e7b97e5fd885598ef197a0";
+    //待研究！
+   
     
 
 }
