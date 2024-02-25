9cbe049aa231bf062afb9cc0e5a5024d2cef5bc6
Big666676 <136260020+Big666676@users.noreply.github.com>
2024-02-25 12:12:54 +0800

    更新:jwt產生器及GITiNgore

---
+++ src/main/java/tw/pers/jwt/demo/controller/UserController.java
@@ -0,0 +1,39 @@
+package tw.pers.jwt.demo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tw.pers.jwt.demo.domain.UserBean;
+import tw.pers.jwt.demo.service.JwtGernater;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private JwtGernater jwtGernater;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> userLogin(@RequestBody UserBean request){
+        System.out.println(request);
+        String s = jwtGernater.generateToken(request);
+        return ResponseEntity.ok().body(s);
+
+    }
+
+    @PostMapping("/check")
+    public ResponseEntity<?> checkValid(@RequestHeader("Authorization") String asd){
+//        System.out.println(userBean);
+        System.out.println(asd);
+        String jwtToken=asd.substring(7).trim();
+        System.out.println(jwtToken);
+        jwtGernater.isVaild(asd);
+        return ResponseEntity.ok().body("SSS");
+
+    }
+
+}
--- src/main/java/tw/pers/jwt/demo/service/JwtGernater.java
+++ src/main/java/tw/pers/jwt/demo/service/JwtGernater.java
@@ -1,13 +1,46 @@
 package tw.pers.jwt.demo.service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tw.pers.jwt.demo.domain.UserBean;
+
+import java.security.Key;
+import java.util.Date;
 
 @Service
+@Transactional
 public class JwtGernater {
-    private final static String SECRET_KEY="443185454aa1d224ae5fa6a04847d7c690abe7eac0e7b97e5fd885598ef197a0";
-    //待研究！
-   
-    
+    private final static String SECRET_KEY = "443185454aa1d224ae5fa6a04847d7c690abe7eac0e7b97e5fd885598ef197a0";
+
+
+    //用來生成token
+    public String generateToken(UserBean userBean) {
+        return Jwts.builder()
+                .claim("userId", userBean.getUserId())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+30*60*1000))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+
+    public boolean isVaild(String token) {
+           Jwts.parser()
+                  .verifyWith(getSignKey())
+                  .build()
+                  .parse(token);
+//          System.out.println(payload);
+       return true;
+      }
+
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
 }
