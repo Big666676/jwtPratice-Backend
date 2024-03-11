ee263da3d81ba7678208b6bd9de6430be13462ac
big67666 <qd3111555@gmail.com>
2024-03-12 00:57:43 +0800

    更新[filter]JwtFilter
    更新如果token過期返回http碼404

--- src/main/java/tw/pers/jwt/demo/filter/JwtFilter.java
+++ src/main/java/tw/pers/jwt/demo/filter/JwtFilter.java
@@ -3,6 +3,7 @@ package tw.pers.jwt.demo.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -18,9 +19,15 @@ public class JwtFilter implements Filter{
     @Override
     public void doFilter(ServletRequest servletRequest,ServletResponse servletResponse,FilterChain filterChain) throws IOException, ServletException{
         HttpServletRequest request=(HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         String token=request.getHeader("Authorization");
+        //檢查是否有token
         if(StringUtils.hasText(token)){
-            System.out.println(token);
+            //檢查token是否還有效
+            if(!jwtUtil.isVaild(token.substring(7).trim())){
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Resource not found");
+                return;
+            }
         }
         filterChain.doFilter(servletRequest,servletResponse);
     }
